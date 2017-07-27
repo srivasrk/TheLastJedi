8459b8361c75f8a427620bc0bddeee90e22fe0a4
Rahul Srivastava <srivasrk@mail.uc.edu>
2017-07-26 22:48:29 -0400

    Adding life to components - basic

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
+  character;
+
   constructor() { }
 
   ngOnInit() {
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsComponent implements OnInit {
 
+  characters = [
+    { name: 'Luke Skywalker', side: ''},
+    { name: 'Darth Vader', side: ''}
+  ];
+
+  chosenList = 'all';
+
+  onChoose(side) {
+    this.chosenList =  side;
+  }
+
+  getCharacters() {
+
+    if (this.chosenList === 'all') {
+      return this.chosenList.slice();
+    }
+
+    return this.characters.filter((character) => {
+      return character.side === this.chosenList;
+    })
+  }
+
+
   constructor() { }
 
   ngOnInit() {
